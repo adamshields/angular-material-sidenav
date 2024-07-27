7edb1a530c9550c5648f0db0042fd21c6b22639d
Adam Shields <adamenote@gmail.com>
2024-07-27 13:49:45 -0400

    Initial Commit

---
+++ src/app/components/custom-sidenav/custom-sidenav.component.ts
@@ -0,0 +1,51 @@
+import { Component, computed, Input, signal } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+export type MenuItem = {
+  icon: string;
+  label: string;
+  route: string;
+}
+
+@Component({
+  selector: 'app-custom-sidenav',
+  standalone: true,
+  imports: [CommonModule, MatListModule, MatIconModule, RouterModule],
+  templateUrl: './custom-sidenav.component.html',
+  styleUrl: './custom-sidenav.component.scss'
+})
+export class CustomSidenavComponent {
+
+  sideNavCollapsed = signal(false);
+  @Input() set collapsed(val: boolean) {
+    this.sideNavCollapsed.set(val);
+  }
+
+  profilePicSize = computed(() => this.sideNavCollapsed() ? '32' : '100');
+
+  menuItems = signal<MenuItem[]>([
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: 'dashboard'
+    },
+    {
+      icon: 'video_library',
+      label: 'Content',
+      route: 'content'
+    },
+    {
+      icon: 'analytics',
+      label: 'Analytics',
+      route: 'analytics'
+    },
+    {
+      icon: 'comment',
+      label: 'Comments',
+      route: 'comments'
+    },
+  ])
+}
