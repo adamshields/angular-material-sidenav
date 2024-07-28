6e9171686f208be6711578a67189c9a3417d4e8a
Adam Shields <adamenote@gmail.com>
2024-07-27 20:17:35 -0400

    added responsive not used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { CustomSidenavComponent } from "./components/custom-sidenav/custom-sidenav.component";
+import { ResponsiveService } from './services/responsive.service';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -18,4 +19,20 @@ export class AppComponent {
   collapsed = signal(false);
 
   sidenavWidth = computed(() => this.collapsed() ? '65px' : '250px');
+
+//   responsiveService = inject(ResponsiveService);
+
+//   sideNavMenuMode = computed(() => {
+//     if (this.responsiveService.largeWidth()) {
+//       return 'side';
+//     }
+//     return 'over';
+//   })
+
+//   mainSideNavMenuMode = computed(() => {
+//     if (this.responsiveService.smallWidth()) {
+//       return 'over';
+//     }
+//     return 'side';
+//   })
 }
